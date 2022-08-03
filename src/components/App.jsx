7590328882355de1a552ae9011d2613764b602c8
Ihor Khomenko <igorhomenko97@gmail.com>
2022-08-03 17:13:15 +0300

    start hw
    added components, added fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import Searchbar from './searchbar/searchbar';
+// import ImageGallery from './components/imageGallery';
+
+class App extends Component {
+  state = {
+    key: '25076604-b2f6a049f29fb1061528c9102'
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.currentTarget.elements.search.value;
+    e.currentTarget.reset()
+    fetch(`https://pixabay.com/api/?q=${searchValue}&page=1&key=${this.state.key}&image_type=photo&orientation=horizontal&per_page=12`).then(res => res.json()).then(data => console.log(data))
+  }
+  render () {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmit}/>
+        {/* <ImageGallery /> */}
+      </div>
+
+    );
+  }
+  
 };
+
+export default App;
---
+++ src/components/imageGallery/imageGallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import ImageGalleryItem from '../imageGalleryItem'
+
+const ImageGallery = () => {
+    return (
+        <ul class="gallery">
+            <li class="gallery-item">
+                <img src="" alt="" />
+            </li>
+        </ul>
+    )
+        
+}
+
+export default ImageGallery;
