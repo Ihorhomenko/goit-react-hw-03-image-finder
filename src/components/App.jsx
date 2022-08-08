db366f7c818fecb61390afa663b9ed752a1b7ba5
Ihor Khomenko <igorhomenko97@gmail.com>
2022-08-08 20:32:12 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,32 +4,33 @@ import ImageGallery from './imageGallery/imageGallery';
 import Button from './button/button';
 import { BallTriangle } from  'react-loader-spinner'
 import Modal from './modal/modal';
+import ApiImages from 'services/services';
 import '../index.css'
 
 class App extends Component {
   state = {
-    key: '25076604-b2f6a049f29fb1061528c9102',
     searchValue: '',
     largeUrl: '',
     gallery: [],
     loader: false,
     page: 1
   }
+
   
 handleSubmit = (e) => {
     e.preventDefault()
     const searchValue = e.currentTarget.elements.search.value;
-    this.setState()
+    this.setState({searchValue})
     e.currentTarget.reset()
     this.setState({gallery: [], loader: true})
-    setTimeout(() => fetch(`https://pixabay.com/api/?q=${searchValue}&page=1&key=${this.state.key}&image_type=photo&orientation=horizontal&per_page=12`).then(res => {if(res.ok){return res.json()} return Promise.reject(new Error('Nothing found for your request'))}).then(data => this.setState({gallery: data.hits})).finally(() => this.setState({loader: false})), 500)
+    ApiImages.fetchImages(searchValue, this.state.page).then(data => this.setState({gallery: data.hits})).finally(() => this.setState({loader: false}))
+    this.setState(prevState => ({ page: prevState.page + 1}))
   }
 
 handleButoonClick = () => {
-    console.log('click')
     const {searchValue, page} = this.state
+    ApiImages.fetchImages(searchValue, page).then(data => this.setState(prevState => ({gallery: [...prevState.gallery, ...data.hits]})))
     this.setState(prevState => ({ page: prevState.page + 1}))
-    fetch(`https://pixabay.com/api/?q=${searchValue}&page=${page}&key=${this.state.key}&image_type=photo&orientation=horizontal&per_page=12`).then(res => res.json()).then(data => this.setState(prevState => ({gallery: [...prevState.gallery, ...data.hits]})))
   }
 
 handleModalClick = (largeUrl) => {
